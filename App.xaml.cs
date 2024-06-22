a008b50d17a7700ff2675a04598652eee7b1c1bb
josephcuenco <josephcuenco@ufl.edu>
2024-06-23 03:05:40 +0900

    start at login page set up

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,17 @@
-﻿namespace KeroKero;
+﻿using KeroKero.Pages;
+
+namespace KeroKero;
 
 public partial class App : Application
 {
+
     public App()
     {
         InitializeComponent();
 
-        MainPage = new AppShell();
+        //MainPage = new AppShell();
+        MainPage = new NavigationPage(new LoginPage());
+        //Shell.Current.GoToAsync("//LoginPage");
 
-        Shell.Current.GoToAsync("//LoginPage");
     }
 }
