48f9da32b4a35fa1581622ce218e5b3d7c1e95af
josephcuenco <josephcuenco@ufl.edu>
2024-06-26 15:25:51 +0900

    update to shell navigation

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,9 +9,24 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        //MainPage = new AppShell();
-        MainPage = new NavigationPage(new LoginPage());
-        //Shell.Current.GoToAsync("//LoginPage");
+        MainPage = new AppShell();
 
+        Routing.RegisterRoute("MainPage", typeof(KeroKero.Pages.MainPage));
+        Routing.RegisterRoute("LoginPage", typeof(KeroKero.Pages.LoginPage));
+        Routing.RegisterRoute("SignUpPage", typeof(KeroKero.Pages.SignUpPage));
+        Routing.RegisterRoute("MapPage", typeof(KeroKero.Pages.MapPage));
+        Routing.RegisterRoute("InfoPage", typeof(KeroKero.Pages.InfoPage));
     }
+        protected override void OnStart()
+        {
+        // Set the initial route to login
+        Shell.Current.GoToAsync("//LoginPage");
+        }
+
+    protected override void OnSleep() { }
+
+    protected override void OnResume() { }
+
+
 }
+
--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -7,12 +7,6 @@ namespace KeroKero
         public AppShell()
         {
             InitializeComponent();
-
-            Routing.RegisterRoute("MainPage", typeof(KeroKero.Pages.MainPage));
-            Routing.RegisterRoute("LoginPage", typeof(KeroKero.Pages.LoginPage));
-            Routing.RegisterRoute("SignUpPage", typeof(KeroKero.Pages.SignUpPage));
-            Routing.RegisterRoute("MapPage", typeof(KeroKero.Pages.MapPage));
-            Routing.RegisterRoute("InfoPage", typeof(KeroKero.Pages.InfoPage));
         }
     }
 }
