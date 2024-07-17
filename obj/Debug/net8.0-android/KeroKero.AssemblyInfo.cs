93eadf936c24bb39adc950967e6026ee2dab2213
joii.d.0648@gmail.com <davis.jcharles>
2024-07-17 12:36:24 -0400

    local

--- obj/Debug/net8.0-android/KeroKero.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/KeroKero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeroKero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ab6aacfec3ef935e130e1419f66e528ec17acab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a6025dbed51d88ae169b1aabcc2719ae8f61915")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeroKero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeroKero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
