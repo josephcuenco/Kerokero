950021c91b241f3a861e3e222291a8248c9ec38e
joii-d <joii.d.0648@gmail.com>
2024-07-21 12:10:32 -0400

    Trying another commit for the phonebook page

---
+++ ViewModels/PhonebookViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Firebase.Auth;
+using KeroKero.Pages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeroKero.ViewModels
+{
+    internal class PhonebookViewModel : INotifyPropertyChanged
+    {
+        //private INavigation _navigation;
+        private string userEmail;
+        private string userPassword;
+        public string webApiKey = "AIzaSyCPz-5MixGymeUJlMKwkyhpZ9ynIGTxIRM";
+
+        public Command ReturnHomeBtn { get; }
+        public Command DialNumberBtn { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public PhonebookViewModel()
+        {
+            //this._navigation = navigation;
+            ReturnHomeBtn = new Command(ReturnHomeTappedAsync);
+            DialNumberBtn = new Command<string>(async (number) => await DialNumberTappedAsync(number));
+        }
+
+        private async void ReturnHomeTappedAsync(object obj)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+
+        private async Task DialNumberTappedAsync(string number)
+        {
+            if (PhoneDialer.Default.IsSupported)
+                PhoneDialer.Default.Open(number);
+        }
+
+    }
+}
