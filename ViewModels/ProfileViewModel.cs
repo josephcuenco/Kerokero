f0e5378a5fbce263e9f092a8364c655690b9a25b
sassygabyrose <msgabyrsmith@gmail.com>
2024-07-24 20:02:24 +0900

    Profile

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 
+
+
 namespace KeroKero.ViewModels
 {
     internal class ProfileViewModel : INotifyPropertyChanged
